e0b67d91f3a6fcfb9f0a3a9d214760ddec4b2995
Yves Siegrist <Elektron1c97@gmail.com>
2018-09-05 14:28:52 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+module.exports = (api, options) => {
+  const { info, chalk, execa } = require('@vue/cli-shared-utils');
+  const { URL } = require('url');
+  const { join } = require('path');
+  const portfinder = require('portfinder');
+
+  api.registerCommand('test:e2e:django', {
+    description: 'run e2e tests with Cypress and django',
+    usage: 'vue-cli-service test:e2e:django [options]',
+    options: {
+      '--headless': 'run in headless mode without GUI',
+      '--mode': 'specify the mode the dev server should run in. (default: production)',
+      '--url': 'run e2e tests against given url instead of auto-starting dev server',
+      '-s, --spec': '(headless only) runs a specific spec file. defaults to "all"'
+    },
+    details:
+    `All Cypress CLI options are also supported:\n` +
+      chalk.yellow(`https://docs.cypress.io/guides/guides/command-line.html#cypress-run`)
+  }, async (args, rawArgs) => {
+    removeArg(rawArgs, 'headless', 0);
+    removeArg(rawArgs, 'mode');
+    removeArg(rawArgs, 'url');
+
+    info('Starting django server...');
+
+    const cwd = join(api.service.context, '..');
+
+    const env = Object.assign({}, process.env);
+    env.DJANGO_ENV = 'TestE2E';
+
+    const backend = execa(
+      'bin/manage.py',
+      ['runserver', process.env.BACKEND_PORT],
+      { env, cwd, detached: true }
+    );
+
+    process.on('SIGINT', () => {
+      process.kill(-backend.pid);
+    });
+
+    info('Starting e2e tests...');
+
+    const { url, server } = args.url
+          ? { url: args.url }
+          : await api.service.run('serve');
+
+    const cyArgs = [
+      args.headless ? 'run' : 'open', // open or run
+      '--config', `baseUrl=${url}`,
+      ...rawArgs
+    ];
+
+    const cypressBinPath = require.resolve('cypress/bin/cypress');
+    const runner = execa(cypressBinPath, cyArgs, { stdio: 'inherit' });
+    if (server) {
+      runner.on('exit', () => {
+	process.kill(-backend.pid);
+	server.close();
+      });
+      runner.on('error', () => {
+	process.kill(-backend.pid);
+	server.close();
+      });
+    }
+
+    if (process.env.VUE_CLI_TEST) {
+      runner.on('exit', code => {
+        process.exit(code);
+      });
+    }
+
+    return runner;
+  });
+};
+
+module.exports.defaultModes = {
+  'test:e2e:django': 'production'
+};
+
+function removeArg (rawArgs, arg, offset = 1) {
+  const matchRE = new RegExp(`^--${arg}`);
+  const equalRE = new RegExp(`^--${arg}=`);
+  const i = rawArgs.findIndex(arg => matchRE.test(arg));
+  if (i > -1) {
+    rawArgs.splice(i, offset + (equalRE.test(rawArgs[i]) ? 0 : 1));
+  }
+}
